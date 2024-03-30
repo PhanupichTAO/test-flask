4f31eb1a1aeb410072027013527f9e65101079cd
PhanupichTAO <143331416+PhanupichTAO@users.noreply.github.com>
2024-03-30 20:20:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,17 @@ app = Flask(__name__)
 
 
 
-@app.route('/', methods=['GET'])
-def index():
-    # หน้า page หลัก
-    return render_template('index.html')
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':         # รับไฟล์ของ post จาก index.html 
     return None
-    
+
+@app.route('/', methods=['GET'])
+def index():
+    # หน้า page หลัก
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
 
