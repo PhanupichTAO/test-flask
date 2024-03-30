562f147f6e3cf11e26de1f64dcf50e4ffd42abe5
PhanupichTAO <phanuphich.ta@rmuti.ac.th>
2024-03-30 20:37:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request, render_template
 app = Flask(__name__)
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
-    # if request.method == 'POST':         # รับไฟล์ของ post จาก index.html 
-    #     delete_image() 
-    #     f = request.files['file']        # เซฟรูปภาพไว้ที่ folder /uploads
-    #     basepath = os.path.dirname(__file__)
-    #     file_path = os.path.join(basepath, 'static/uploads', secure_filename(f.filename))
-    #     f.save(file_path) 
+    if request.method == 'POST':         # รับไฟล์ของ post จาก index.html 
+        # delete_image() 
+        # f = request.files['file']        # เซฟรูปภาพไว้ที่ folder /uploads
+        # basepath = os.path.dirname(__file__)
+        # file_path = os.path.join(basepath, 'static/uploads', secure_filename(f.filename))
+        # f.save(file_path) 
 
-    #     remove_background()
+        # remove_background()
 
-    #     crop_image(file_path)
-    #     results, accuracies, accuracy_percentage = model_predict()  # Get predictions and accuracies
-    #     img = show_image()
-    #     data = [{'result': result, 'accuracy': accuracy, 'accuracy_average': accuracy_average, 'img_file': img_file}
-    #             for result, accuracy, accuracy_average, img_file in zip(results, accuracies, accuracy_percentage, img)]
+        # crop_image(file_path)
+        # results, accuracies, accuracy_percentage = model_predict()  # Get predictions and accuracies
+        # img = show_image()
+        # data = [{'result': result, 'accuracy': accuracy, 'accuracy_average': accuracy_average, 'img_file': img_file}
+        #         for result, accuracy, accuracy_average, img_file in zip(results, accuracies, accuracy_percentage, img)]
 
-    #     return jsonify(data)  # Return JSON response containing results and accuracies
+        # return jsonify(data)  # Return JSON response containing results and accuracies
+        return None
 
     return None
 
