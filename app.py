1d2fe4f3052c98a88eee0da80d7287d758c6f95e
PhanupichTAO <143331416+PhanupichTAO@users.noreply.github.com>
2024-03-30 19:56:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World! 5555555555"
+
+@app.route('/', methods=['GET'])
+def index():
+    # หน้า page หลัก
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
