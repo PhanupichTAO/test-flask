ca1083c8ba831a6706c422c237d166deebea4f4d
PhanupichTAO <phanuphich.ta@rmuti.ac.th>
2024-04-20 21:21:47 +0700

    ฟ

--- app.py
+++ app.py
@@ -9,10 +9,10 @@
 # import cv2
 
 
-import os
-import glob
+# import os
+# import glob
 from flask import Flask, jsonify, request, render_template
-from werkzeug.utils import secure_filename
+# from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__, static_url_path='/static')
@@ -51,28 +51,28 @@ def index():
     # หน้า page หลัก
     return render_template('index.html')
 
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':         # รับไฟล์ของ post จาก index.html 
-        # delete_image() 
-        # f = request.files['file']        # เซฟรูปภาพไว้ที่ folder /uploads
-        # basepath = os.path.dirname(__file__)
-        # file_path = os.path.join(basepath, 'static/uploads', secure_filename(f.filename))
-        # f.save(file_path) 
+# @app.route('/predict', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST':         # รับไฟล์ของ post จาก index.html 
+#         # delete_image() 
+#         # f = request.files['file']        # เซฟรูปภาพไว้ที่ folder /uploads
+#         # basepath = os.path.dirname(__file__)
+#         # file_path = os.path.join(basepath, 'static/uploads', secure_filename(f.filename))
+#         # f.save(file_path) 
 
-        # remove_background()
+#         # remove_background()
   
 
-        # crop_image(file_path)
-        # results, accuracies, accuracy_percentage = model_predict()  # Get predictions and accuracies
-        # img = show_image()
-        # data = [{'result': result, 'accuracy': accuracy, 'accuracy_average': accuracy_average, 'img_file': img_file}
-        #         for result, accuracy, accuracy_average, img_file in zip(results, accuracies, accuracy_percentage, img)]
+#         # crop_image(file_path)
+#         # results, accuracies, accuracy_percentage = model_predict()  # Get predictions and accuracies
+#         # img = show_image()
+#         # data = [{'result': result, 'accuracy': accuracy, 'accuracy_average': accuracy_average, 'img_file': img_file}
+#         #         for result, accuracy, accuracy_average, img_file in zip(results, accuracies, accuracy_percentage, img)]
 
-        # return jsonify(data)  # Return JSON response containing results and accuracies
-        return True
+#         # return jsonify(data)  # Return JSON response containing results and accuracies
+#         return True
 
-    return None
+#     return None
 
 
 
