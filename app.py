6945f3fe96994f124f3d35c2ec88be5045ea6472
PhanupichTAO <phanuphich.ta@rmuti.ac.th>
2024-04-21 15:10:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 # from PIL import Image
 
 #แก้ไข library เพื่อให้สามารถ deploy ได้
-# from keras.models import_ load_model
+from keras.models import load_model
 import numpy as np
 import cv2
 
@@ -17,28 +17,28 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__, static_url_path='/static')
 
-# model = load_model('model/model__data_aug_BGR_RGB.h5') #ใช้อันนี้
-# model.make_predict_function()
+model = load_model('model/model__data_aug_BGR_RGB.h5') #ใช้อันนี้
+model.make_predict_function()
 
 #ลบพื้นหลังของภาพ
-# def remove_background():
-#         files = glob.glob(os.path.join('static/uploads/*'))
-#         for file in files:
-#             imgInput = cv2.imread(file)
-#             imgInput = cv2.resize(imgInput, (300, 300))
-#             height, width = imgInput.shape[:2]
-#             mask = np.zeros(imgInput.shape[:2], np.uint8)
-#             backgroundModel = np.zeros((1, 65), np.float64)
-#             forgroundModel = np.zeros((1, 65), np.float64)
-#             rect = (10, 10, width-30, height-30)
-#             cv2.grabCut(imgInput, mask, rect, backgroundModel,
-#                         forgroundModel, 5, cv2.GC_INIT_WITH_RECT)
-#             mask = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
-#             image_1 = imgInput*mask[:, :, np.newaxis]
-#             background = imgInput - image_1
-#             background[np.where((background > [0, 0, 0]).all(axis=2))] = [0, 0, 0]
-#             imgFinal = background + image_1
-#             cv2.imwrite(file,imgFinal)     
+def remove_background():
+        files = glob.glob(os.path.join('static/uploads/*'))
+        for file in files:
+            imgInput = cv2.imread(file)
+            imgInput = cv2.resize(imgInput, (300, 300))
+            height, width = imgInput.shape[:2]
+            mask = np.zeros(imgInput.shape[:2], np.uint8)
+            backgroundModel = np.zeros((1, 65), np.float64)
+            forgroundModel = np.zeros((1, 65), np.float64)
+            rect = (10, 10, width-30, height-30)
+            cv2.grabCut(imgInput, mask, rect, backgroundModel,
+                        forgroundModel, 5, cv2.GC_INIT_WITH_RECT)
+            mask = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+            image_1 = imgInput*mask[:, :, np.newaxis]
+            background = imgInput - image_1
+            background[np.where((background > [0, 0, 0]).all(axis=2))] = [0, 0, 0]
+            imgFinal = background + image_1
+            cv2.imwrite(file,imgFinal)     
 
 
 #ลบไฟล์รูปภาพจากโฟลเดอร์ uploads  
@@ -57,7 +57,7 @@ def upload():
         file_path = os.path.join(basepath, 'static/uploads', secure_filename(f.filename))
         f.save(file_path) 
 
-        # remove_background()
+        remove_background()
   
 
         # crop_image(file_path)
