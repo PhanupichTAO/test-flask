3df50327054024c39a3b22551824d0fd04d3c126
PhanupichTAO <phanuphich.ta@rmuti.ac.th>
2024-04-21 11:08:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def upload():
         #         for result, accuracy, accuracy_average, img_file in zip(results, accuracies, accuracy_percentage, img)]
 
         # return jsonify(data)  # Return JSON response containing results and accuracies
-        return True
+        # return True
 
     return None
 
